bb050dee38af338f5c2d8bd98dcd977111187ea8
Salih041 <salihozbk41@gmail.com>
2025-11-30 19:25:40 +0300

    Update mail settings

--- utils/email.js
+++ utils/email.js
@@ -3,19 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sendEmail = async (options)=>{
+const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service : 'Gmail',
-        auth : {
-            user:process.env.EMAIL_USER,
-            pass : process.env.EMAIL_PASS
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
     const mailOptions = {
-        from : `"SelamY Blog" <${process.env.EMAIL_USER}>`,
-        to : options.email,
-        subject : options.subject,
+        from: `"SelamY Blog" <${process.env.EMAIL_USER}>`,
+        to: options.email,
+        subject: options.subject,
         html: options.html
     };
 
